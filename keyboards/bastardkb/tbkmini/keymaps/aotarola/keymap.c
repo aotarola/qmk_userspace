6fa01e0c3b6732388be98bb0ea6e318692eb182d
Andres Otarola <andres@otarola.me>
2025-03-02 15:29:31 -0800

    fix

--- keyboards/bastardkb/tbkmini/keymaps/aotarola/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/aotarola/keymap.c
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------|.                   |------------------------------------------------------|
-            KC_TAB,   KC_A,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_Y,    KC_BACKSLASH,
+            KC_TAB,   KC_A,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_Y,    KC_BSLS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
-            KC_LCTL,  KC_A,    KC_R,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+            KC_ESC,  KC_A,    KC_R,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
             KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
-                                       KC_LGUI, KC_SPC,   KC_SPC,                        KC_ENT,   KC_ENT,  KC_RALT
+                                       KC_LGUI, KC_LSFT,   KC_SPC,                        KC_ENT,   KC_RSFT,  KC_RALT
                                    //`--------------------------'                    `--------------------------'
 
         ),
