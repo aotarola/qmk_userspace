bb16054065577a5b17ed340e221ce1e77240ea02
Andres Otarola <andres@otarola.me>
2025-03-02 21:07:46 -0800

    fix L1

--- keyboards/bastardkb/tbkmini/keymaps/aotarola/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/aotarola/keymap.c
@@ -19,8 +19,8 @@
 #define SLSH_RCTL MT(MOD_RCTL, KC_SLSH)
 #define DEL_ALT MT(MOD_RALT, KC_DEL)
 #define BSC_SFT MT(MOD_RSFT, KC_BSPC)
-#define SPC_L1 MT(MO(1), KC_SPC)
-#define ENT_L2 MT(MO(2), KC_ENT)
+#define SPC_L1 LT(1, KC_SPC)
+#define ENT_L2 LT(2, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -32,23 +32,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
             KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                          KC_K,    KC_M,    KC_COMM, KC_DOT, SLSH_RCTL, KC_EQL,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
-                                       KC_LGUI, KC_LSFT, SPC_L1,                         ENT_L2, BSC_SFT,  DEL_ALT
+                                       KC_LGUI, KC_LSFT, SPC_L1,                        ENT_L2, BSC_SFT,  DEL_ALT
                                    //`--------------------------'                    `--------------------------'
 
         ),
 
+    // L1
     [1] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------|.                   |------------------------------------------------------|
             KC_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,   KC_7,    KC_8,    KC_9,    KC_0,   KC_BSPC,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
-            KC_LCTL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, XXXXXXX,
+            KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDC,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
-            KC_LSFT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
                                       KC_LGUI,  KC_SPC, _______,                         MO(3), KC_ENT, KC_RALT
                                    //`--------------------------'                    `--------------------------'
         ),
 
+    // L2
     [2] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------|.                   |------------------------------------------------------|
              KC_TAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
