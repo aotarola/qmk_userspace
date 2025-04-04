d15402a0655cd417d1c0765714fbb84bb46b5a33
Andres Otarola <andres@otarola.me>
2025-04-03 18:20:14 -0700

    add volumen control and F12

--- keyboards/bastardkb/tbkmini/keymaps/aotarola/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/aotarola/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
             KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
-            KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_VOLD, XXXXXXX, XXXXXXX,  KC_F12, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+---------|
                                       KC_LGUI,  KC_SPC, _______,                         MO(3), KC_ENT, KC_RALT
                                    //`--------------------------'                    `--------------------------'
